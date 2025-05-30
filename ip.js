80e19f53f304bcac9cb9eef22754b4504c28d3fe
Eduard8813 <143306086+Eduard8813@users.noreply.github.com>
2025-05-30 01:44:59 -0600

    Update ip.js

--- ip.js
+++ ip.js
@@ -1,8 +1,8 @@
-const ip = ''
-const token = '1d45f466a0f167'
+const ip = '45.170.226.90';
+const token = '1d45f466a0f167';
 
-const url = 'https://ipinfo.io/${ip}?token=${token}'
+const url = `https://ipinfo.io/${ip}?token=${token}`;
 
-const res = await fetch(url)
-const json = await res.json()
-console.log(json)
+const res = await fetch(url);
+const json = await res.json();
+console.log(json);  
